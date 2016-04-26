12e6d4fafc5922f9e21d49427f1debc08811e878
RodrigoAlbert <rodrigo.albert@outlook.com>
2016-04-25 21:19:45 -0300

    Terminando crud

--- Store/DAL/Persistence/ProdutoDAL.cs
+++ Store/DAL/Persistence/ProdutoDAL.cs
@@ -30,6 +30,60 @@ namespace DAL.Persistence
 
                 throw new Exception(ex.Message);
             }
+            finally
+            {
+                FechaConexao();
+            }
+        }
+
+        public void Deletar(int Id)
+        {
+            try
+            {
+                if (AbreConexao())
+                {
+                    Cmd = new SqlCommand("delete from Produtos where Id=@id",Con);
+                    Cmd.Parameters.AddWithValue("@id",Id);
+
+                    Cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                FechaConexao();
+            }
+        }
+
+        public void Atualizar(Produto p)
+        {
+            try
+            {
+                if (AbreConexao())
+                {
+                    Cmd = new SqlCommand("update Produtos set (Nome=@nome, Area=@area, Quantidade=@quantidade, Modelo=@modelo) where Id=@id",Con);
+                    Cmd.Parameters.AddWithValue("@id", p.Id);
+                    Cmd.Parameters.AddWithValue("@nome", p.Nome);
+                    Cmd.Parameters.AddWithValue("@quantidade", p.Quantidade);
+                    Cmd.Parameters.AddWithValue("@modelo", p.Modelo);
+                    Cmd.Parameters.AddWithValue("@area", p.Area);
+
+                    Cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                FechaConexao();
+            }
         }
     }
 }
